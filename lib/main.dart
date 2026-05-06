ab77a170fd93cbdee649ebb5196b9a1bcb6406b0
barq <barqaday@gmail.com>
2026-05-06 22:19:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ void main() async {
 
   // تأكد من وضع بياناتك هنا ليعمل الربط الحقيقي
   await Supabase.initialize(
-    url: 'https://YOUR_PROJECT_URL.supabase.co',
-    anonKey: 'YOUR_ANON_KEY',
+    url: 'https://jmsmrojtlstppnpwmkkk.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos',
   );
 
   runApp(const MyApp());
